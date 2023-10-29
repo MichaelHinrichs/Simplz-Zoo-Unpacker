61e652eb06ca710258b54c1fba63dfc9129859b4
MichaelHinrichs <mchinrichs@gmail.com>
2023-10-29 14:26:30 -0500

    Nevermind
    Apparently, the input variable NEEDS to be an array names "args" for some dumb reason.

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ namespace Simplz_Zoo_Unpacker
         public static BinaryReader br;
         public static BinaryWriter bw;
 
-        static void Main(string file)
+        static void Main(string[] args)
         {
-            br = new BinaryReader(File.OpenRead(file));
+            br = new BinaryReader(File.OpenRead(args[0]));
             string type = Encoding.ASCII.GetString(br.ReadBytes(br.ReadInt32()));
-            Directory.CreateDirectory(Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file));
+            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]));
             System.Collections.Generic.List<Subfile> subfiles = new();
             while (br.BaseStream.Position < br.BaseStream.Length - 4)
             {
                 subfiles.Add(new());
-                Directory.CreateDirectory(Path.GetDirectoryName(Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + "\\" + subfiles[^1].subfileName));
-                bw = new BinaryWriter(File.OpenWrite(Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + "\\" + subfiles[^1].subfileName + ".zlib"));
+                Directory.CreateDirectory(Path.GetDirectoryName(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]) + "\\" + subfiles[^1].subfileName));
+                bw = new BinaryWriter(File.OpenWrite(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]) + "\\" + subfiles[^1].subfileName + ".zlib"));
                 bw.Write(br.ReadBytes(subfiles[^1].size));
                 bw.Close();
             }
