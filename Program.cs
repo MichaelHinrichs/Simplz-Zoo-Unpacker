817578bd1915995ca3b92a2103b7c2bc16b6d431
MichaelHinrichs <mchinrichs@gmail.com>
2023-10-29 14:16:07 -0500

    Change input variable
    It doesn't need more that 1 input anyway.

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ namespace Simplz_Zoo_Unpacker
         public static BinaryReader br;
         public static BinaryWriter bw;
 
-        static void Main(string[] args)
+        static void Main(string file)
         {
-            br = new BinaryReader(File.OpenRead(args[0]));
+            br = new BinaryReader(File.OpenRead(file));
             string type = Encoding.ASCII.GetString(br.ReadBytes(br.ReadInt32()));
-            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]));
+            Directory.CreateDirectory(Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file));
             System.Collections.Generic.List<Subfile> subfiles = new();
             while (br.BaseStream.Position < br.BaseStream.Length - 4)
             {
                 subfiles.Add(new());
-                Directory.CreateDirectory(Path.GetDirectoryName(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]) + "\\" + subfiles[^1].subfileName));
-                bw = new BinaryWriter(File.OpenWrite(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]) + "\\" + subfiles[^1].subfileName + ".zlib"));
+                Directory.CreateDirectory(Path.GetDirectoryName(Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + "\\" + subfiles[^1].subfileName));
+                bw = new BinaryWriter(File.OpenWrite(Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + "\\" + subfiles[^1].subfileName + ".zlib"));
                 bw.Write(br.ReadBytes(subfiles[^1].size));
                 bw.Close();
             }
